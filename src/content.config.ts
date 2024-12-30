77c4bc491aa81f52fb8920ce1586572dc0ef1023
Philippe LeSaux <lesauxp@gmail.com>
2024-12-30 16:49:18 -0500

    update from legacy content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const albums = defineCollection({
-    type: 'data',
+    loader: glob({ pattern: '**/*.yaml', base: './src/content/albums' }),
     schema: (context) =>
         z.object({
             title: z.string(),
