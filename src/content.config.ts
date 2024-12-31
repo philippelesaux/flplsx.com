060b1fdd4d2e98767db37e2bccc21757b71e9aae
Philippe LeSaux <lesauxp@gmail.com>
2024-12-31 16:51:06 -0500

    use new collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { file } from 'astro/loaders';
 
-const albums = defineCollection({
-    loader: glob({ pattern: '**/*.yaml', base: './src/content/albums' }),
-    schema: (context) =>
-        z.object({
-            title: z.string(),
-            description: z.string().nullable(),
-            cover: context.image()
-        }),
+const portfolio = defineCollection({
+    loader: file('src/data/portfolio.json'),
+    schema: ({ image }) => z.object({
+        id: z.string(),
+        title: z.string(),
+        location: z.string(),
+        image: image(),
+        alt: z.string(),
+    })
 });
 
 export const collections = {
-    albums,
+    portfolio
 };
