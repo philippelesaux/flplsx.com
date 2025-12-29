687f7cb6c0abd7e7e6996eff6bc6ec261de92143
Philippe LeSaux <lesauxp@gmail.com>
2025-12-28 19:15:30 -0500

    swap default page

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,5 +1,19 @@
-export default function Lightbox() {
+import type { AllImages } from "../pages/index.astro";
+
+export default function Lightbox({ allImages }: { allImages: AllImages }) {
     return (
-        <h1>I did it!</h1>
-    )
+        <div className="p-2 mx-auto container gap-2 my-8 columns-2 md:columns-3 lg:columns-4 xl:columns-5">
+            {
+                allImages.map((entry) => (
+                    <img
+                        src={entry.image.src}
+                        alt={entry.alt}
+                        width={entry.image.width}
+                        className="rounded mb-1 border border-transparent hover:border-gray-300 transition-all duration-300 ease-in-out hover:shadow-lg"
+                        loading="lazy"
+                    />
+                ))
+            }
+        </div>
+    );
 }
